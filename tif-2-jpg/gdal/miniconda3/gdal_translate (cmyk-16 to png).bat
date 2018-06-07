c85e6f546a492ff232c0f8efbc1eb14c2e1b7e83
Noreh AD <akinoreh@gmail.com>
2018-06-07 11:21:38 +0300

    .

---
+++ tif-2-jpg/gdal/miniconda3/gdal_translate (cmyk-16 to png).bat
@@ -0,0 +1,7 @@
+@echo off
+
+cd "C:\ProgramData\Miniconda3\Library\bin"
+
+gdal_translate -of PNG "C:\Users\%username%\Documents\GitHub\dump\tif-2-jpg\images\cmyk-16.tif" "C:\Users\%username%\Documents\GitHub\dump\tif-2-jpg\gdal\miniconda3\output\cmyk-16.png"
+
+pause
