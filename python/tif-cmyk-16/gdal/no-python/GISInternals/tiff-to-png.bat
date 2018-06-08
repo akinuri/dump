4c730e9329efd2605d47cc3f16a1c0c5ece05782
Noreh AD <akinoreh@gmail.com>
2018-06-08 15:19:23 +0300

    .

--- python/tif-cmyk-16/gdal/no-python/GISInternals/tiff-to-png.bat
+++ python/tif-cmyk-16/gdal/no-python/GISInternals/tiff-to-png.bat
@@ -1,5 +1,5 @@
 @echo off
 
-"C:\Program Files\GDAL\gdal_translate.exe" -scale -ot byte -of PNG "C:\Users\%username%\Documents\GitHub\dump\tif-2-jpg\images\cmyk-16.tif" "cmyk-16.png"
+"C:\Program Files\GDAL\gdal_translate.exe" -scale -ot byte -of PNG "C:\Users\%username%\Documents\GitHub\dump\python\tif-cmyk-16\images\cmyk-16.tif" "cmyk-16.png"
 
 pause
