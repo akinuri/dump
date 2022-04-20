523b69531bdfc3fb3c604430e96c05792bafa6cb
Noreh AD <akinoreh@gmail.com>
2022-04-20 16:02:04 +0300

    updated expired url

--- m4s-segments/1-download-master-json.py
+++ m4s-segments/1-download-master-json.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-master_json_url = "https://10vod-adaptive.akamaized.net/exp=1650461792~acl=%2F74595329-11a7-4673-9a2a-4d4842f8eb89%2F%2A~hmac=3959646d1a911b3ab3d96037a2e8d2dcc2ef4b1522d3dbcf148971e8166d97ad/74595329-11a7-4673-9a2a-4d4842f8eb89/sep/video/36f891d2,48eb6c54,22a5b384,e7f0c05b,be7925bd/audio/3befbd40,8bb0aa93/master.json?query_string_ranges=1&base64_init=1"
+master_json_url = "https://10vod-adaptive.akamaized.net/exp=1650463490~acl=%2F74595329-11a7-4673-9a2a-4d4842f8eb89%2F%2A~hmac=1188bba8adc8dd948beeee283cfc127d3554840078b4bbf0394e08ddcc789241/74595329-11a7-4673-9a2a-4d4842f8eb89/sep/video/36f891d2,48eb6c54,22a5b384,e7f0c05b,be7925bd/audio/3befbd40,8bb0aa93/master.json?query_string_ranges=1&base64_init=1"
 master_json_url_response = requests.get(master_json_url)
 master_json = master_json_url_response.json()
 
